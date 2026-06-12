e83b914f3888b0de1b671e4d8cb3e703c33d2fd1
Jorge Garcia <jorgegarcia1@lithia.com>
2026-06-12 13:37:22 -0600

    working on server file

--- server/server.js
+++ server/server.js
@@ -6,7 +6,11 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 
-const app = express().use(cors(corsOptions)).use(express.json()).use(cookieParser());
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {console.log("TEST snowflake Server started on port " + PORT)});
