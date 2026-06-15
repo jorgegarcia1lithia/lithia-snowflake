b1efb0b43990089c049a8c626f947f74eded07c3
Jorge Garcia <jorgegarcia1@lithia.com>
2026-06-15 15:21:35 -0600

    working on slitnow

--- server/server.js
+++ server/server.js
@@ -7,82 +7,76 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const snowflake = require('snowflake-sdk');
 const crypto = require('crypto');
-
-const privateKeyFile = Buffer.from(process.env.privateKey, 'base64').toString('utf-8');
-
-const privateKeyObject = crypto.createPrivateKey({
-    key: privateKeyFile,
-    format: 'pem',
-    passphrase: process.env.passphrase
-});
-
-// Extract the private key from the object as a PEM-encoded string.
-const privateKey = privateKeyObject.export({
-    format: 'pem',
-    type: 'pkcs8'
-});
   
-// Use the private key for authentication.
-const connection = snowflake.createConnection({
-    account: process.env.account,
-    username: process.env.user,
-    role: process.env.role,
-    authenticator: 'SNOWFLAKE_JWT',
-    privateKey: privateKey
-});
-  
-// Establish a connection.
-connection.connect((err, conn) => {
-    if (err) {
-        // Handle any errors.
-        console.log("Error TEST JG:");
-        console.log(err);
-    } else {
-        const statement = connection.execute({
-            sqlText: "CALL MARKETING_PROD.BIZ.SP_CUSTOMER_360_LOOKUP(P_VIN => '5FNRL6H78JB025038');",
-            asyncExec: true,
-            complete: async function (err, stmt, rows) {
-                if (err) {
-                    console.error(`Failed to execute statement due to the following error: ${err.message}`);
-                } else {
-                    console.log(`Successfully executed statement: ${stmt.getSqlText()}`);
-                    console.log(`rows: ${stmt.getSqlText()}`);
-
-                    const queryId = stmt.getQueryId();
-
-                    connection.getResultsFromQueryId({
-                        queryId: queryId,
-                        complete: async function (err, _stmt, rows) {
-                            console.log(rows);
-                        }
-                    });
-                }
-            }
-        });
-        //console.log("statement JG:");
-        //console.log(statement);
-    }
-});
-  
-// Execute SQL statements.
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    //res.send('Hello World!');
 });
 
 app.post('/slitnow', (req, res) => {
-    const userData = req.body;
+    const requestData = req.body;
 
-    console.log(userData); 
+    const privateKeyFile = Buffer.from(process.env.privateKey, 'base64').toString('utf-8');
 
-    // Send a success response back to the client
-    res.status(201).json({
-        message: 'User created successfully',
-        data: userData
+    const privateKeyObject = crypto.createPrivateKey({
+        key: privateKeyFile,
+        format: 'pem',
+        passphrase: requestData.passphrase
+    });
+
+    // Extract the private key from the object as a PEM-encoded string.
+    const privateKey = privateKeyObject.export({
+        format: 'pem',
+        type: 'pkcs8'
+    });
+    
+    // Use the private key for authentication.
+    const connection = snowflake.createConnection({
+        account: requestData.account,
+        username: requestData.user,
+        role: requestData.role,
+        authenticator: requestData.authenticator,
+        privateKey: privateKey
+    });
+    
+    // Establish a connection.
+    connection.connect((err, conn) => {
+        if (err) {
+            // Handle any errors.
+            console.log("Error TEST JG:");
+            console.log(err);
+        } else {
+            const statement = connection.execute({
+                sqlText: requestData.sqlText,
+                asyncExec: true,
+                complete: async function (err, stmt, rows) {
+                    if (err) {
+                        console.error(`Failed to execute statement due to the following error: ${err.message}`);
+                    } else {
+                        console.log(`Successfully executed statement: ${stmt.getSqlText()}`);
+                        console.log(`rows: ${stmt.getSqlText()}`);
+
+                        const queryId = stmt.getQueryId();
+
+                        connection.getResultsFromQueryId({
+                            queryId: queryId,
+                            complete: async function (err, _stmt, rows) {
+                                //console.log(rows);
+
+                                // Send a success response back to the client
+                                res.status(201).json({
+                                    data: rows
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+        }
     });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {console.log("TEST snowflake Server started on port " + PORT)});
+app.listen(PORT, () => {console.log("snowflake middleware Server started on port " + PORT)});
